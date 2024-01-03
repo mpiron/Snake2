aa741e59736f3c1ca50b3b3ef817b9bfd9908a18
mpiron <murielle.piron@gmail.com>
2024-01-03 21:49:08 +0100

    Travail non réalisé Selon le désir des élèves, nous le ferons peut être... # augmenter la taille du serpent # vérifier la collision

--- jeu_snake.py
+++ jeu_snake.py
@@ -1,7 +1,6 @@
-# SNAKE 0.5 :
-# finalement le système de déplacement n'est pas bon car dans snake,
-# on donne une direction et le serpent avance jusqu'à ce qu'on
-# change de direction, nous corrigeons ça!
+# SNAKE 0.6 :  Travail en attente
+# augmenter la taille du serpent
+# vérifier la collision 
 from class_snake import Snake  # Tous les fichiers en minuscules, les classes avec 1 majuscule
 import random  # 0.1
 import pygame  # 0
@@ -14,7 +13,7 @@ velocity = 30
 apple_x = random.randint(15, screen_width-15)  # 0.1
 apple_y = random.randint(15, screen_height-15)  # 0.1
 
-pygame.display.set_caption("Jeu Snake 4TT (version 0.5)")  # 0
+pygame.display.set_caption("Jeu Snake 4TT (version 0.6)")  # 0
 # screen est une variable qui correspond à une surface,
 screen = pygame.display.set_mode((screen_width, screen_height))  # 0.1
 running = True  # 0
@@ -33,7 +32,7 @@ screen.blit(apple, (apple_x, apple_y))  # 0.1
 pygame.display.flip()  # 0.1
 
 while running:  # 0
-    screen.fill((0, 0, 0))
+    screen.fill((0,0,0))
     screen.blit(apple, (apple_x, apple_y))
     pygame.draw.ellipse(screen, (20, 190, 20), [
         snake.x, snake.y, snake.width, snake.width])  # 0.3
@@ -44,9 +43,9 @@ while running:  # 0
             running = False  # 0
             pygame.quit()  # 0
         elif event.type == pygame.KEYDOWN:   # 0.2
-            snake.pressed = event.key
-        # à partir de maintenant, je sais quelle touche
-        # vient d'être enfoncée
+            snake.pressed=event.key
+        # quelle touche vient d'être enfoncée
+
     # vérifier si le joueur veut déplacer le serpent.
     if snake.pressed == pygame.K_RIGHT:                     # 0.5
         snake.x += velocity                                 # 0.5
@@ -56,4 +55,4 @@ while running:  # 0
         snake.y -= velocity                                 # 0.5
     elif snake.pressed == pygame.K_DOWN:                    # 0.5
         snake.y += velocity                                 # 0.5
-    pygame.time.delay(100)                                  # 0.4
+    pygame.time.delay(100)                                  # 0.4 
