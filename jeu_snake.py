c40500bbdd84231282d8e6cf3c11e5e384729645
mpiron <murielle.piron@gmail.com>
2024-01-03 21:55:50 +0100

    version fonctionnelle initiale (merci de Graven) mars 2022 ...

--- jeu_snake.py
+++ jeu_snake.py
@@ -1,58 +1,145 @@
-# SNAKE 0.6 :  Travail en attente
-# augmenter la taille du serpent
-# vérifier la collision 
-from class_snake import Snake  # Tous les fichiers en minuscules, les classes avec 1 majuscule
-import random  # 0.1
-import pygame  # 0
-pygame.init()  # 0
+version fonctionnelle initiale (merci de Graven) mars 2022 ...
+import pygame
+import random
+pygame.init()#commandes de jeu qui vont arriver
 
-# initialisation variables
-screen_width = 960  # 0.1
-screen_height = 720  # 0.1
-velocity = 30
-apple_x = random.randint(15, screen_width-15)  # 0.1
-apple_y = random.randint(15, screen_height-15)  # 0.1
+plat_largeur = 1500#largeur du plateau
+plat_hauteur = 900#hauteur du plateau
+#Important, cette variable sera sans doute très important par la suite. 
+plateau = pygame.display.set_mode((plat_largeur,plat_hauteur))#création du plateau
+pygame.display.set_caption("Snake Le Jeu de Mme Piron")#Nom du jeu
+#info_ecran=pygame.display.info()  trouvé par Anton pour une affichage en plein écran
+#https://stackoverflow.com/questions/19954469/how-to-get-the-resolution-of-a-monitor-in-pygame
 
-pygame.display.set_caption("Jeu Snake 4TT (version 0.6)")  # 0
-# screen est une variable qui correspond à une surface,
-screen = pygame.display.set_mode((screen_width, screen_height))  # 0.1
-running = True  # 0
+ColorSnake = (60, 60, 90)
+ColorFond = (232, 255, 0)
+ColorEcriture = (0, 0, 0)
+ColorPomme = (60, 60, 90)
+Jaune = (255, 255, 200)
+Rouge = (255, 200, 200)
+Vert = (200, 255, 150)
+Bleu = (200, 200, 255)
+Rose = (255, 200, 255)
 
-# charger le serpent
-snake = Snake(screen, screen_width/2, screen_height/2)
+liste_couleur = [Jaune, Rouge, Vert, Bleu, Rose]
+
+#X1 et y1 donnent la position du snake au démarrage (au milieu)
+#... erreur de ma part, à comprendre plus tard
+x1 = plat_largeur/2
+y1 = plat_hauteur/2
+
+vitesse = 5
+bloc_serpent = 50
+
+pomme = pygame.image.load("pomme.png").convert_alpha()
+#ajout _alpha pour obtenir une transparence du PNG
+pommetaille = pygame.transform.scale(pomme, (bloc_serpent,bloc_serpent))
+
+x1_change = 0
+y1_change = 0
+ 
+#à expliquer, quel est l'intérêt???
 clock = pygame.time.Clock()
 
+police_ecriture = pygame.font.SysFont("Arial",60)
 
-# La variable apple contiendra l'image de la pomme avec ses dimentions
-apple = pygame.image.load("pomme.png")  # 0.1
-apple = pygame.transform.scale(apple, (30, 30))  # 0.1
-# blit() = injecter une image à un endroit spécifique
-screen.blit(apple, (apple_x, apple_y))  # 0.1
-# Flip() =  mettre à jour.
-pygame.display.flip()  # 0.1
+def message(msg, color):
+    mesg = police_ecriture.render(msg, True, color)
+    dimension = mesg.get_rect()
+    plateau.blit(mesg,[plat_largeur/2-dimension.width/2, plat_hauteur/2-dimension.height/2])
+    
+def mon_score(score):
+    score_py = police_ecriture.render("Points : " + str(score), True, (150,150,150))
+    plateau.blit(score_py, [0,0])
+    
+def mon_serpent(liste_serpent,color):
+    for carre in liste_serpent:
+        pygame.draw.rect(plateau, color, [carre[0], carre[1], bloc_serpent, bloc_serpent])
+    
+    
+def gameLoop():
+    game_over = False
+    game_stop = False
+    x1 = plat_largeur/2
+    y1 = plat_hauteur/2
+    x1_change = 0
+    y1_change = 0
+    couleurSerpent = 0 #numéro dans la liste des couleurs 
+    score = 0
+    foodx = round(random.randrange(0, plat_largeur-bloc_serpent)/bloc_serpent)*bloc_serpent
+    foody = round(random.randrange(0, plat_hauteur-bloc_serpent)/bloc_serpent)*bloc_serpent
 
-while running:  # 0
-    screen.fill((0,0,0))
-    screen.blit(apple, (apple_x, apple_y))
-    pygame.draw.ellipse(screen, (20, 190, 20), [
-        snake.x, snake.y, snake.width, snake.width])  # 0.3
-    pygame.display.flip()  # 0.2
+    r = 0
+    g = 255
+    b = 0
 
-    for event in pygame.event.get():  # 0
-        if event.type == pygame.QUIT:  # 0
-            running = False  # 0
-            pygame.quit()  # 0
-        elif event.type == pygame.KEYDOWN:   # 0.2
-            snake.pressed=event.key
-        # quelle touche vient d'être enfoncée
+    liste_serpent =[]
+    longueur_serpent = 1
+    while not game_over:
+        multicolore = (r,g,b)
+        while game_stop==True:
+            plateau.fill(ColorFond)
+            message("Dommage -_-      ESPACE = rejouer     ESCAPE = quitter", ColorEcriture)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game_over = True
+                        game_stop = False
+                    if event.key == pygame.K_SPACE:
+                        gameLoop()
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                game_over = True
+                
+            if event.type == pygame.KEYDOWN:    
+                if event.key==pygame.K_UP:
+                    x1_change = 0
+                    y1_change =-bloc_serpent
+                if event.key==pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = bloc_serpent            
+                if event.key==pygame.K_LEFT:
+                    x1_change =-bloc_serpent
+                    y1_change = 0            
+                if event.key==pygame.K_RIGHT:
+                    x1_change = bloc_serpent
+                    y1_change = 0
+        if y1<0 or x1>=plat_largeur or x1<0 or y1>=plat_hauteur:
+            game_stop = True
+        x1 += x1_change
+        y1 += y1_change   
+        
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, plat_largeur-bloc_serpent)/bloc_serpent)*bloc_serpent
+            foody = round(random.randrange(0, plat_hauteur-bloc_serpent)/bloc_serpent)*bloc_serpent
+            score += 1  
+            longueur_serpent += 1
+            couleurSerpent += 1
+        plateau.fill(liste_couleur[couleurSerpent%len(liste_couleur)])
+        # pygame.draw.rect(plateau,ColorPomme ,[foodx, foody, bloc_serpent, bloc_serpent])
+        pomme_rect = pommetaille.get_rect(topleft=(foodx,foody))
+        plateau.blit(pommetaille, (pomme_rect))
+        tete_serpent = []
+        tete_serpent.append(x1)
+        tete_serpent.append(y1)
+        liste_serpent.append(tete_serpent)
+        if len(liste_serpent) > longueur_serpent:
+            del liste_serpent[0]
+        for carre in liste_serpent[:-1]:
+            if carre == tete_serpent:
+                game_stop = True
+    
+        
+        mon_serpent(liste_serpent,multicolore)
+        mon_score(score)
+        pygame.display.update()
+        
+        clock.tick(vitesse)    
+        
 
-    # vérifier si le joueur veut déplacer le serpent.
-    if snake.pressed == pygame.K_RIGHT:                     # 0.5
-        snake.x += velocity                                 # 0.5
-    elif snake.pressed == pygame.K_LEFT:                    # 0.5
-        snake.x -= velocity                                 # 0.5
-    elif snake.pressed == pygame.K_UP:                      # 0.5
-        snake.y -= velocity                                 # 0.5
-    elif snake.pressed == pygame.K_DOWN:                    # 0.5
-        snake.y += velocity                                 # 0.5
-    pygame.time.delay(100)                                  # 0.4 
+ 
+
+    pygame.quit()
+    
+gameLoop()
