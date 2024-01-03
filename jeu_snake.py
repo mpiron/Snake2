c78ab7bcf592ddae9a5d89e458ab843fb20902d8
mpiron <murielle.piron@gmail.com>
2024-01-03 21:43:22 +0100

    version 5:  finalement le système de déplacement n'est pas bon car dans snake, on donne une direction et le serpent avance jusqu'à ce qu'on change de direction

--- jeu_snake.py
+++ jeu_snake.py
@@ -1,10 +1,7 @@
-# SNAKE 0.4 : création de fichiers séparés pour les classes
-# + gestion des touches qui restent enfoncées (ca ne sera finalement pas utile).
-# https://youtu.be/a0kcj6rRQ2s?t=745
-# https://youtu.be/a0kcj6rRQ2s?t=1183
-# finalement ce système de déplacement n'est pas bon car dans snake,
+# SNAKE 0.5 :
+# finalement le système de déplacement n'est pas bon car dans snake,
 # on donne une direction et le serpent avance jusqu'à ce qu'on
-# change de direction, nous allons corriger ca dans le 0.5
+# change de direction, nous corrigeons ça!
 from class_snake import Snake  # Tous les fichiers en minuscules, les classes avec 1 majuscule
 import random  # 0.1
 import pygame  # 0
@@ -17,7 +14,7 @@ velocity = 30
 apple_x = random.randint(15, screen_width-15)  # 0.1
 apple_y = random.randint(15, screen_height-15)  # 0.1
 
-pygame.display.set_caption("Jeu Snake 4TT (version 0.4)")  # 0
+pygame.display.set_caption("Jeu Snake 4TT (version 0.5)")  # 0
 # screen est une variable qui correspond à une surface,
 screen = pygame.display.set_mode((screen_width, screen_height))  # 0.1
 running = True  # 0
@@ -36,7 +33,8 @@ screen.blit(apple, (apple_x, apple_y))  # 0.1
 pygame.display.flip()  # 0.1
 
 while running:  # 0
-
+    screen.fill((0, 0, 0))
+    screen.blit(apple, (apple_x, apple_y))
     pygame.draw.ellipse(screen, (20, 190, 20), [
         snake.x, snake.y, snake.width, snake.width])  # 0.3
     pygame.display.flip()  # 0.2
@@ -46,18 +44,16 @@ while running:  # 0
             running = False  # 0
             pygame.quit()  # 0
         elif event.type == pygame.KEYDOWN:   # 0.2
-            snake.pressed[event.key] = True
-        elif event.type == pygame.KEYUP:
-            snake.pressed[event.key] = False
-        # a partir de maintenant, je sais quelles touches
-        # sont actionnées par le joueur.
+            snake.pressed = event.key
+        # à partir de maintenant, je sais quelle touche
+        # vient d'être enfoncée
     # vérifier si le joueur veut déplacer le serpent.
-    if snake.pressed.get(pygame.K_RIGHT):
-        snake.x += velocity
-    elif snake.pressed.get(pygame.K_LEFT):
-        snake.x -= velocity
-    elif snake.pressed.get(pygame.K_UP):
-        snake.y -= velocity
-    elif snake.pressed.get(pygame.K_DOWN):
-        snake.y += velocity
-    pygame.time.delay(100)
+    if snake.pressed == pygame.K_RIGHT:                     # 0.5
+        snake.x += velocity                                 # 0.5
+    elif snake.pressed == pygame.K_LEFT:                    # 0.5
+        snake.x -= velocity                                 # 0.5
+    elif snake.pressed == pygame.K_UP:                      # 0.5
+        snake.y -= velocity                                 # 0.5
+    elif snake.pressed == pygame.K_DOWN:                    # 0.5
+        snake.y += velocity                                 # 0.5
+    pygame.time.delay(100)                                  # 0.4
